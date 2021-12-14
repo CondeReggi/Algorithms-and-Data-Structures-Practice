7369fe58d8128e2e926ea86766f85a44c24ed4f0
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2021-12-13 23:38:59 -0300

    Solution function

--- codewars.js
+++ codewars.js
@@ -484,6 +484,44 @@ function anagrams(word, words) {
   return arrAux
 }
 
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in. 
+// Any whitespace at the end of the line should also be stripped out.
+
+// Given an input string of:
+
+// apples, pears # and bananas
+// grapes
+// bananas !apples
+
+// The output expected would be:
+
+// apples, pears
+// grapes
+// bananas
+
+function solution(input, markers) {
+  let isFounded = false;
+  let letters = input.split('');
+  let stripped = letters.reduce((acc , letter) => {
+    if(markers.includes(letter)) isFounded = true;
+    if(isFounded && letter === '\n') isFounded = false;
+    if(!isFounded) acc.push(letter)
+      return acc;
+  }, [])
+  
+  return stripped.join('').replace(/\s\n/ , '\n').trim();
+};
+
+function solution(input, markers) {
+  return input.split('\n').map(
+    line => markers.reduce(
+      (line, marker) => line.split(marker)[0].trim(), line
+    )
+  ).join('\n')
+}
+
+//Trim sirve para que no hayan espacios --> ' hoo     ' => 'hoo' 
+
 
 
 
