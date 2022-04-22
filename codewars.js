0baf1580f8457d6fe871cbd5f044f165e5352df8
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2022-04-22 15:58:28 -0300

    Mas funciones
    sortArray

--- codewars.js
+++ codewars.js
@@ -1061,7 +1061,7 @@ function incrementString (strng) {
 
 // sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
-//If there are no numbers of this kind in the range [a, b] the function should output an empty list.
+// If there are no numbers of this kind in the range [a, b] the function should output an empty list.
 // sumDigPow(90, 100) == []
 
 function sumDigPow(a, b) {
@@ -1078,6 +1078,14 @@ const calculateProperty = (number) => {
   return sum;
 }
 
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
-
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+  return array.map((x) => x % 2 ? odd.shift() : x);
+}
 
