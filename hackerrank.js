c28c1243056faaa96a09c97486a7650b404ceebf
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2022-05-18 10:23:43 -0300

    More functions
    birthdayCakeCandles, timeConversion

--- hackerrank.js
+++ hackerrank.js
@@ -157,3 +157,16 @@ function miniMaxSum(arr) {
     }   
     console.log(min, max);
 }
+
+function birthdayCakeCandles(candles) {
+    let max = Math.max(...candles);
+    return candles.filter(x => x == max).length;
+}
+
+function timeConversion(s) {
+    const arr = s.slice(0,8).split(':');
+    arr[0] = (s.indexOf('PM') > -1) ?
+            (arr[0] == 12 ? '12' : Number(arr[0]) + 12) :
+            (arr[0] == 12 ? '00' : arr[0]);
+    return arr.join(':');
+}
