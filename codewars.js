e7ecb5985d63c17a88f53d66caf22ae730398b68
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2022-06-21 12:17:57 -0300

    UriBuilder
    UriBuilder

--- codewars.js
+++ codewars.js
@@ -1742,6 +1742,38 @@ function filter_list_v2(l) {
   return arrayToReturn;
 }
 
+//var builder = new UriBuilder('http://www.codewars.com')
+//builder.params.page = 1
+//builder.params.language = 'javascript'
 
+// new builder instance to demonstrate pre-existing params.
+//builder = new UriBuilder('http://www.codewars.com?page=1')
 
+//builder.params.page = 2
+// should return 'http://www.codewars.com?page=2'
+// builder.build()
+
+// if you delete params then they will disappear from the url string
+// delete builder.params.page
+
+// should return 'http://www.codewars.com'
+// builder.build()
+
+function UriBuilder(fullUrl){
+  const [url, allParams] = fullUrl.split('?');
+  this.url = url;
+  
+  this.params = allParams.split('&').reduce((acc, curr) => {
+    const [clave, valor] = curr.split('=');
+    
+    return {
+      ...acc,
+      [clave]: valor
+    }
+  }, {});
+  
+  this.build = function(){
+    return this.url + '?' + Object.keys(this.params).map(x => `${x}=${this.params[x]}`).join('&');
+  }
+}
 
