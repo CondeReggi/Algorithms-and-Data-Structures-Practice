ddea307e43c9fa300759b0addc96a08c676658b3
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2022-07-14 17:42:51 -0300

    reverse
    reverse

--- codewars.js
+++ codewars.js
@@ -2023,4 +2023,10 @@ function doubles(maxk, maxn) {
     return result;
 }
 
+// No time for stories. Reverse an array, return the result. Do whatever you want with the original array. Don't use Array.prototype.reverse.
+// You have 30 bytes to spare.
+// Example: [1, 2, 3] → [3, 2, 1]
+
+const reverse = a => [...a].map(a.pop ,a)
+
 
