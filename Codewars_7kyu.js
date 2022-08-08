c32539ad02fef8efecfe7653602b6b4dc5a7b1ad
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2022-08-08 11:55:05 -0300

    moveTen and more
    moveTen

--- Codewars_7kyu.js
+++ Codewars_7kyu.js
@@ -126,3 +126,89 @@ function minValue(values){
   return parseInt(unique.sort((a,b) => a - b).reduce((acc, res) => acc + res, ""))
 }
 
+/*-All unique-*/
+
+function hasUniqueChars(str){
+  const array = Array.from(str);
+  return array.every(x => array.indexOf(x) === array.lastIndexOf(x))
+}
+
+/*-Chuck Norris I - Push Ups-*/
+
+function chuckPushUps(string) {
+  if(string === 1 || !string || string.length === 0) return 'FAIL!!'
+  let binary = []
+  
+  if(Array.isArray(string)){
+    binary = string.filter(x => x.includes('1') || x.includes('0'));
+    
+    if(binary.length === 0) 
+      return 'FAIL!!';
+  }else{
+    if(Array.from(string).indexOf(' ') === -1){
+      
+      binary = string.toString().split('').filter(x => x.includes('1') || x.includes('0')).join('');
+      const value = parseInt(binary, 2)
+      return !isNaN(string) ? 'FAIL!!' : value
+    }else{
+      binary = string.split(' ').filter(x => x.includes('1') || x.includes('0'));
+    }
+  }
+  
+  if(binary.length === 0) return 'CHUCK SMASH!!';
+  
+  const numbers = binary.map(x => parseInt(x, 2));
+  
+  return Math.max(...numbers)
+}
+
+/*-Chuck Norris II - One Punch-*/
+
+function onePunch(items){
+  const result =  items.big && items.length ? items.split(' ').sort().join(' ').replace(/[ae]/ig,'') : "Broken!";
+  return result;
+  
+  const removeAorR = items.split(' ').map(x => {
+    const value = x.replace(/[a,e]/gi, '');
+    const sum = x.length - value.length
+    
+    return {
+      value,
+      sum
+    }
+  });
+
+  const sort = removeAorR.sort((a, b) => {
+    if(b.sum - a.sum === 1){
+      return a.value - b.value
+    }else{
+      return b.sum - a.sum;
+    }
+  });
+  
+  return sort.map(x => x.value).join(' ');
+}
+
+/*-Shortest Word-*/
+
+function findShort(s){
+  const results = s.split(' ').map(x => x.length);
+  return Math.min(...results)
+}
+
+/*-Move 10-*/
+
+function moveTen(s){
+  const array = s.split('').map(x => {
+    const charZ = "z";
+    if(x.charCodeAt(0) > charZ.charCodeAt(0)){
+      const value = x.charCodeAt(0) - charZ;
+      return 'a'.charAt(0) + value;
+    }else{
+      return x.charCodeAt(0)
+    }
+  });
+  
+  return array.map(x => x.toString().charAt(0)).join('')
+}
+
