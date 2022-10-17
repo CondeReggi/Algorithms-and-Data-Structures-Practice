9dfbe457d66525dbd1109f295fd565294125bc75
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2022-10-17 17:00:45 -0300

    Convert to Binary
    Convert to Binary

--- 8_de_kyu.cs
+++ 8_de_kyu.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 public static class Kata
 {
@@ -41,6 +42,79 @@ public static class Kata
     }
     
     return result;
+  }
+  
+  // Find Nearest square number
+  
+  public static int NearestSq(int n)
+  {
+    var sqrt = Math.Sqrt(n);
+    return (int)Math.Pow(Math.Round(sqrt), 2);
+  }
+  
+  // Multiple of index
+  
+  public static List<int> MultipleOfIndex(List<int> xs)
+  {
+    List<int> result = new List<int>(){};
+    for(int i = 1; i < xs.Count; i++){
+      if(xs[i] % i == 0){
+        result.Add(xs[i]);
+      }
+    }
+    return result;
+    
+    return xs
+      .Skip(1)
+      .Where((x, i) => x % (i + 1) == 0)
+      .ToList();
+  }
+  
+  // CSV representation of array
+  
+  public static string ToCsvText(int[][] array)
+  {
+    List<string> result = new List<string>(){};
+    
+    for(int i = 0; i < array.Length ; i++){
+      result.Add( String.Join(',' , array[i]) );
+    } 
+    
+    return String.Join('\n', result);
+  }
+  
+  // Multiplication table for number
+  
+  public static string MultiTable(int number)
+  {
+    List<string> result = new List<string>(){};
+    for(int i = 1; i <= 10; i++){
+      result.Add( $"{i} * {number} = {number * i}" );
+    }
+    return String.Join('\n', result);
+  }
+  
+  // Convert to Binary
+
+  public static int ToBinary(int n)
+  {
+    return Int32.Parse(Convert.ToString(n, 2));
+  }
+  
+  // I love you, a little , a lot, passionately ... not at all
+
+  public static string HowMuchILoveYou(int nb_petals)
+  {
+    string[] results = new string[]{
+      "not at all",
+      "I love you",
+      "a little",
+      "a lot",
+      "passionately",
+      "madly"
+    };
+    
+    return results[nb_petals % 6];
   }
 }
 
