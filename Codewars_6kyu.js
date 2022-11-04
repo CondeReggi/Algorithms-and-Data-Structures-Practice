347f8e2a6366c7cdf106f628a396c46a216e8496
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2022-11-04 10:38:24 -0300

    Are they the "same"?
    Are they the "same"?

--- Codewars_6kyu.js
+++ Codewars_6kyu.js
@@ -556,4 +556,28 @@ function collatz(n){
   return arr.join("->")
 }
 
+/*-Are they the "same"?-*/
 
+function comp(array1, array2){
+  if(!array1 || !array2) return false
+  if(array1.length === 0 || array2.length === 0) return true
+  
+  array1 = array1.sort((a,b) => a - b);
+  array2 = array2.sort((a,b) => a - b);
+  while(array1.length > 0 && array2.length > 0){
+    if(array1.shift() !== Math.sqrt(array2.shift())){
+      return false;
+    }
+  }
+  return true
+}
+
+/*-N-th Fibonacci-*/
+
+function nthFibo(n) {
+  let [prev, curr] = [0, 1];
+  for (let i = 1; i < n; i++) [prev, curr] = [curr, prev + curr];
+  return prev;
+}
+
+/*--*/
