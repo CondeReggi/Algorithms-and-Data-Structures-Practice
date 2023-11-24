fb1cb0e3528f6e2efd41541aeb5236a071ae9ffb
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-11-24 11:08:43 -0300

    problemas tenemos todos

--- Leetcode.cs
+++ Leetcode.cs
@@ -296,6 +296,26 @@ public class Solution {
         return result;
     }
 
+    public long MinimumSteps(string s) {
+        int n = s.Length;
+        long blackCount = 0;
+        long steps = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1')
+            {
+                blackCount++;
+            }
+            else
+            {
+                steps += blackCount;
+            }
+        }
+
+        return steps;
+    }
+
     
 }
 
