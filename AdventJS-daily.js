b73ea4d9e4bce4370497c1537c059662a90641eb
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-06 10:11:12 -0300

    problemas tenemos todos

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -104,3 +104,21 @@ function cyberReindeer(road, time) {
     return resultado;
 }
 
+//DIA 6
+function maxDistance(movements) {
+  let int = 0;
+  return Math.abs(movements.split("").reduce((acc, res) => {
+    switch(res){
+      case '*':
+        int++;
+        break;
+      case '>':
+        acc += 1;
+        break;
+      case '<':
+        acc -= 1;
+        break;
+    }
+    return acc;
+  }, 0)) +int;
+}
