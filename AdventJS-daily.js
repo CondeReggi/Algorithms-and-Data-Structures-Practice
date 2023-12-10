99a9538c26f0cc0df5872df37ee8d2285037fa88
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-10 14:45:15 -0300

    Update AdventJS-daily.js

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -222,3 +222,27 @@ function organizeGifts(gifts) {
     const data = obtenerObjeto(gifts);
     return data;
 }
+
+//DIA 9
+function adjustLights(lights) {
+    const red = '🔴';
+    const green = '🟢';
+
+    let changesForRedStart = 0;
+    let changesForGreenStart = 0;
+
+    for (let i = 0; i < lights.length; i++) {
+        if (i % 2 === 0) { 
+            if (lights[i] !== red) changesForRedStart++;
+            if (lights[i] !== green) changesForGreenStart++;
+        } else { 
+            if (lights[i] !== green) changesForRedStart++;
+            if (lights[i] !== red) changesForGreenStart++;
+        }
+    }
+
+    return Math.min(changesForRedStart, changesForGreenStart);
+}
+
+//DIA 10
+
