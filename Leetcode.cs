c8883560bd7ceed7cac2db6e2129f1b99cc3f9d6
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-10 14:25:17 -0300

    867. Transpose Matrix

--- Leetcode.cs
+++ Leetcode.cs
@@ -448,6 +448,18 @@ public class Solution {
         return (n*(n+1))/2;
     }
 
+    public int[][] Transpose(int[][] matrix) {
+        int[][] result = new int[matrix[0].Length][];
+
+        for (int i = 0; i < matrix[0].Length; i++) {
+            result[i] = new int[matrix.Length];
+            for (int j = 0; j < matrix.Length; j++) {
+                result[i][j] = matrix[j][i];
+            }
+        }
+        return result;
+    }
+    
 }
 
 public class NumArray {
