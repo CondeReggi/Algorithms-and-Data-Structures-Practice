68a3570b688e8a7975bb8d95c25d05712522bad9
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-11 15:31:30 -0300

    Los elfos estudiosos

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -245,4 +245,59 @@ function adjustLights(lights) {
 }
 
 //DIA 10
+function createChristmasTree(ornaments, height) {
+  const result = [];
+  const characters = ornaments.split('');
 
+  for(let i = 0; i < height; i++){
+    let str = "";
+    for(let j = 0; j <= i; j++){
+      const value = characters.shift();
+      characters.push(value);
+      str += value + " ";
+    }
+    result.push(str.trim().padStart(height + i, " "));
+  }
+  result.push('|'.padStart(height, " "))
+  
+  return result.join("\n") + "\n";
+}
+
+//DIA 11
+function getIndexsForPalindrome(word) {
+  const isPalindome = (word) => word.split("")
+  .every((x, i) => x === word[word.length - i - 1]);
+
+  if(isPalindome(word)) return [];
+
+  const splited = word.split("");
+  const splited2 = word.split("");
+
+  for(let i = 0; i < word.length / 2; i++){
+    const {primero , segundo} = {
+      primero: word[i], 
+      segundo: word[word.length - 1 - i]
+    }
+    if(primero != segundo) {
+      const index = splited.indexOf(segundo, i + 1);
+      const index2 = splited.indexOf(primero, i + 1);
+      let bool = true;
+      
+      if(index != -1) {
+          [splited[index], splited[i]] = [splited[i], splited[index]]
+          let esPalindrome = isPalindome(splited.join(""));
+          if(esPalindrome) return [i, index];
+          else bool = bool && !esPalindrome;
+      }
+      if(index2 != -1){
+          [splited2[index2], splited2[word.length - 1 - i]] 
+            = [splited2[word.length - 1 - i], splited2[index2]]
+          let esPalindrome = isPalindome(splited2.join(""))
+          if(esPalindrome) return [index2, word.length - 1 - i];
+          else bool = bool && !esPalindrome;
+      }
+      if(bool) return null;
+    }
+  }
+  return null
+}
