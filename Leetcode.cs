bdab4358b0b142c8908f01b29693e12a04d8fe4d
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-12 09:17:58 -0300

    11. Container With Most Water

--- Leetcode.cs
+++ Leetcode.cs
@@ -459,7 +459,25 @@ public class Solution {
         }
         return result;
     }
-    
+
+    public int MaxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.Length - 1;
+
+        while (left < right) {
+            int width = right - left;
+            int minHeight = Math.Min(height[left], height[right]);
+            maxArea = Math.Max(maxArea, width * minHeight);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
 }
 
 public class NumArray {
