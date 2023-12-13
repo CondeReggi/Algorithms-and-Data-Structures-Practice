e79ea9a829c2fc3ff14559bb1d7a34e30a51aa36
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-13 14:05:34 -0300

    Calculando el tiempo

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -325,3 +325,23 @@ function checkIsValidCopy(original, copy) {
   }
   return bool
 }
+
+//DIA 13
+function calculateTime(deliveries) {
+  let counter = 0;
+  const DATE_DEFAULT = "1999-05-18T";
+  const today = new Date(DATE_DEFAULT + "00:00:00");
+  for(let hours of deliveries){
+    const result = new Date(DATE_DEFAULT + hours).getTime();
+    counter += (result - today.getTime());
+  }
+  const siete = new Date(DATE_DEFAULT + "07:00:00").getTime();
+  today.setMilliseconds(counter);
+  const diff = siete - today.getTime();
+  
+  if(diff > 0){
+    return `-${new Date(diff).toISOString().slice(11, 19)}`
+  }else{
+    return new Date(Math.abs(diff)).toISOString().slice(11, 19)
+  }
+}
