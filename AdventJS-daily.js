cc98794b1e3c2112c6b58c73b76dfb2ccb16bc78
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-18 15:44:03 -0300

    #16 Despliegue en viernes

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -441,3 +441,25 @@ function autonomousDrive(store, movements) {
     return store;
 }
 
+//DIA 16
+function transformTree(tree) {
+  const insertLevelOrder = (arr, i) => {
+    let root = null;
+
+    if (i < arr.length && arr[i] !== null && arr[i] !== undefined) {
+      root = {
+        value: arr[i]
+      };
+
+      root['left'] = insertLevelOrder(arr, 2 * i + 1);
+      root['right'] = insertLevelOrder(arr, 2 * i + 2);
+    }
+    return root;
+  }
+
+  if (tree.length == 0) return null;
+
+  const datos = insertLevelOrder(tree, 0);
+  return datos;
+}
+
