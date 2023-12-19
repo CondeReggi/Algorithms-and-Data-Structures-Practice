edda1f040be6eaa627a2b55cf345c0dafaa28e8d
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-19 12:20:25 -0300

    #18 Y #19

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -489,3 +489,72 @@ function optimizeIntervals(intervals) {
   return result
 }
 
+
+//DIA 18
+function drawClock(time) {
+  const numeros = [
+    ["***", "* *", "* *", "* *", "* *", "* *", "***"], // 0
+    ["  *", "  *", "  *", "  *", "  *", "  *", "  *"], // 1
+    ["***", "  *", "  *", "***", "*  ", "*  ", "***"], // 2
+    ["***", "  *", "  *", "***", "  *", "  *", "***"], // 3
+    ["* *", "* *", "* *", "***", "  *", "  *", "  *"], // 4
+    ["***", "*  ", "*  ", "***", "  *", "  *", "***"], // 5
+    ["***", "*  ", "*  ", "***", "* *", "* *", "***"], // 6
+    ["***", "  *", "  *", "  *", "  *", "  *", "  *"], // 7
+    ["***", "* *", "* *", "***", "* *", "* *", "***"], // 8
+    ["***", "* *", "* *", "***", "  *", "  *", "***"], // 9
+    [" ", " ", "*", " ", "*", " ", " "]
+  ];
+
+  const hora = time.split(":").join("").split("").map(Number);
+    hora.splice(2, 0, 10); // Insertamos el separador
+
+    let resultado = [];
+
+    for (let i = 0; i < 7; i++) {
+        let linea = hora.map(n => numeros[n][i]).join(" ");
+        resultado.push(linea.split(""));
+    }
+
+    return resultado;
+}
+
+//DIA 19
+function revealSabotage(store) {
+  const mapa = store
+    .map(x => x.map(y => y == ' ' ? 0 : y));
+  
+  for(let i = 0; i < mapa.length; i++){
+      for(let j = 0; j < mapa[i].length; j++){
+          if(mapa[i][j] == '*'){
+            if(j + 1 < mapa[i].length && 
+              !isNaN(mapa[i][j + 1])) 
+              mapa[i][j + 1]++; 
+            if(j - 1 >= 0 && !isNaN(mapa[i][j - 1])) 
+              mapa[i][j - 1]++;            
+            if(i - 1 >= 0) {
+                if(!isNaN(mapa[i - 1][j])) mapa[i - 1][j]++;                      
+                if(j + 1 < mapa[i].length 
+                  && !isNaN(mapa[i - 1][j + 1])) 
+                  mapa[i - 1][j + 1]++; 
+                if(j - 1 >= 0 && !isNaN(mapa[i - 1][j - 1])) 
+                  mapa[i - 1][j - 1]++;            
+            }
+            if(i + 1 < mapa.length) {
+                if(!isNaN(mapa[i + 1][j])) mapa[i + 1][j]++;                      
+                if(j + 1 < mapa[i].length 
+                  && !isNaN(mapa[i + 1][j + 1])) 
+                  mapa[i + 1][j + 1]++; 
+                if(j - 1 >= 0 && !isNaN(mapa[i + 1][j - 1])) 
+                  mapa[i + 1][j - 1]++;            
+            }
+          }
+      }
+  }
+ 
+  return mapa.map(x => x.map(y => {
+    if(y == '*') return y;
+    if(y == 0) return ' ';
+    return y.toString()
+  }))
+}
