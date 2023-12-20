02b8a8ce9561537ea549fcab73572173f99aaad5
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-20 12:45:13 -0300

    #20 Distibuye el paso

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -558,3 +558,40 @@ function revealSabotage(store) {
     return y.toString()
   }))
 }
+
+//DIA 20
+function distributeGifts(weights) {
+  const result = [];
+  for(let i = 0; i < weights.length; i++){
+    const array = [];
+    
+    for(let j = 0; j < weights[i].length; j++){
+    let suma = weights[i][j] || 0;
+    let cantidadAyacentes = weights[i][j] ? 1 : 0;
+      //sumo adyacentes izquierda y derecha
+      if(j + 1 < weights[i].length && weights[i][j+1]){
+        cantidadAyacentes++;
+        suma += weights[i][j+1];
+      } 
+      if(j - 1 >= 0 && weights[i][j-1]){
+        cantidadAyacentes++;
+        suma += weights[i][j-1];
+      }
+
+      //sumo adyacentes arriba y abajo
+      if(i + 1 < weights.length && weights[i + 1][j]){
+        cantidadAyacentes++;
+        suma += weights[i + 1][j];
+      }
+      if(i - 1 >= 0 && weights[i - 1][j]){
+        cantidadAyacentes++;
+        suma += weights[i - 1][j];
+      }
+
+      array.push(Math.round(suma / cantidadAyacentes));
+    }
+    result.push(array);
+  }
+
+  return result
+}
