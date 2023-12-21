fb4007b1b6c352c4a21d2ac8ffd3f9f55912abe4
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-21 15:12:42 -0300

    #21 Mensaje binario

--- AdventJS-daily.js
+++ AdventJS-daily.js
@@ -595,3 +595,27 @@ function distributeGifts(weights) {
 
   return result
 }
+
+//DIA 21
+function findBalancedSegment(message) {
+    let [maxLength, startIndex, count] = [0,-1,0]
+    let map = { 0: -1 };
+
+    for (let i = 0; i < message.length; i++) {
+        count += message[i] === 1 ? 1 : -1;
+
+        if (map.hasOwnProperty(count)) {
+            let length = i - map[count];
+            
+            if (length > maxLength) {
+                maxLength = length;
+                startIndex = map[count] + 1;
+            }
+        } else {
+            map[count] = i;
+        }
+    }
+
+    if (startIndex === -1) return [];
+    return [startIndex, startIndex + maxLength - 1];
+}
