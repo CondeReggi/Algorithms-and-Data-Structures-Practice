e6cc6746788bbb3c08407cf71ff7403f5df5b396
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2023-12-28 16:07:39 -0300

    22. Generate Parentheses

--- Leetcode.cs
+++ Leetcode.cs
@@ -885,6 +885,22 @@ public class NumArray {
         }
         return true;
     }
+
+    public IList<string> GenerateParenthesis(int n) {
+        List<string> result = new List<string>();
+        Generate(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void Generate(List<string> result, string current, int open, int close, int max){
+        if(current.Length == 2 * max){
+            result.Add(current);
+            return;
+        }
+
+        if(open < max) Generate(result, current + "(", open + 1, close, max);
+        if(close < open) Generate(result, current + ")", open, close + 1, max);
+    }
 }
 
 public class FoodRatings {
