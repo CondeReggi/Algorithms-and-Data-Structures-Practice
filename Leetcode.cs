cb38a348b656dc3a2fe816e51ca7bb45f673f4a8
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2024-01-02 15:33:43 -0300

    33. Search in Rotated Sorted Array

--- Leetcode.cs
+++ Leetcode.cs
@@ -1105,6 +1105,31 @@ public class NumArray {
         }
         return true;
     }
+
+    public int Search(int[] nums, int target) {
+        if(nums == null || nums.Length == 0) return -1;
+        int start = 0, end = nums.Length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) return mid;
+
+            if(nums[start] <= nums[mid]) {
+                if(target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if(target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
 
 public class FoodRatings {
