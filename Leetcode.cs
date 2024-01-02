08222a58577d9d30bf43bfe32f617b8cd7c47c98
Lucas Conde <59385581+CondeReggi@users.noreply.github.com>
2024-01-02 10:41:38 -0300

    17. Letter Combinations of a Phone Number

--- Leetcode.cs
+++ Leetcode.cs
@@ -979,6 +979,38 @@ public class NumArray {
             current.RemoveAt(current.Count() - 1);
         }
     }
+
+    private Dictionary<int, List<string>> datos = new Dictionary<int, List<string>>(){
+        {2, new List<string>(){ "a", "b", "c" }},
+        {3, new List<string>(){ "d", "e", "f" }},
+        {4, new List<string>(){ "g", "h", "i" }},
+        {5, new List<string>(){ "j", "k", "l" }},
+        {6, new List<string>(){ "m", "n", "o" }},
+        {7, new List<string>(){ "p", "q", "r", "s" }},
+        {8, new List<string>(){ "t", "u", "v" }}, 
+        {9, new List<string>(){ "w", "x", "y", "z" }}
+    };
+
+    public IList<string> LetterCombinations(string digits) {
+        List<string> result = new List<string>();
+        if(String.IsNullOrEmpty(digits)) return result;
+
+        Backtrack(result, "", digits, 0);
+        return result; 
+    }
+
+    private void Backtrack(IList<string> result, string current, string digits, int start){
+        if(current.Length == digits.Length){
+            result.Add(current);
+            return;
+        }
+
+        foreach(var letter in datos[digits[start] - '0']){
+            Backtrack(result, current + letter, digits, start + 1);
+        }
+    }
+
+    
 }
 
 public class FoodRatings {
